ece37cafec6651ee33ad16fb8b80a726aa57f2a5
bimonestle <bimonestle@yahoo.co.id>
2024-01-01 15:50:29 +0700

    First django template (html)

--- calculate_avg_sessions/views.py
+++ calculate_avg_sessions/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 def calc_avg_sess(request):
-    return HttpResponse("Hello, This is Calculate Average Sessions project!")
+    # return HttpResponse("Hello, This is Calculate Average Sessions project!") # testing only
+    template = loader.get_template("my_first.html")
+    return HttpResponse(template.render())
--- my_personal_website/settings.py
+++ my_personal_website/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "calculate_avg_sessions"
 ]
 
 MIDDLEWARE = [
