e61dc7ef06770fdcd4b9759bd4686e7106f7059d
bimonestle <bimonestle@yahoo.co.id>
2024-01-07 14:33:06 +0700

    add root route to personal_app.html

--- personal/urls.py
+++ personal/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('personal_app/', views.personal, name='members'),
+    path('', views.personal, name='personal'),
     path('calculate_average_sessions/', views.calculate_avg_sessions, name='calc_avg_sess')
 ]
