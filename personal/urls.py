5c55a0378ec264e8373e7f1ffd64cd9a08e84e2d
bimonestle <bimonestle@yahoo.co.id>
2024-01-07 14:06:21 +0700

    add link to html template

--- personal/urls.py
+++ personal/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('personal_app/', views.personal, name='members')
+    path('personal_app/', views.personal, name='members'),
+    path('calculate_average_sessions/', views.calculate_avg_sessions, name='calc_avg_sess')
 ]
--- personal/views.py
+++ personal/views.py
@@ -6,3 +6,7 @@ from django.template import loader
 def personal(request):
     template = loader.get_template("personal_app.html")
     return HttpResponse(template.render())
+
+def calculate_avg_sessions(request):
+    template = loader.get_template("calc_avg_sess.html")
+    return HttpResponse(template.render())
